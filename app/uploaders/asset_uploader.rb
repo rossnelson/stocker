c63dec66dc69badbd5c365ce65666dd5fa729f22
Ross Nelson <axcess1@me.com>
2012-12-26 09:11:43 -0600

    img max height and uploads saved to system

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -5,7 +5,7 @@ class AssetUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
 end
